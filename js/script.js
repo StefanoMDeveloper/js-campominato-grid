0ca7247fcd3e7241937701f55e8f3deefe9b2709
StefanoMDeveloper <stenmarti02@gmail.com>
2021-12-21 16:53:39 +0100

    finito. agg active, bombe e punteggio.

--- js/script.js
+++ js/script.js
@@ -6,14 +6,6 @@ con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
-
-// for (let i = 0; i < 10; i++) {
-//     for (let j = 0; j < 10; j++) {
-//         document.writeln(j+1)
-//     }
-//     document.writeln("<br>");
-// }
-
 let bottone = document.getElementById("gioca");
 bottone.addEventListener('click', function(){
     let livello = parseInt(document.getElementById("difficolta").value);
@@ -30,19 +22,80 @@ bottone.addEventListener('click', function(){
 
         case 2:
             for (let i = 1; i <= 81; i++) {
-                contenitore.innerHTML += `<div class="item item2">${i}</div>`
+                contenitore.innerHTML += `<div class="item2 item">${i}</div>`
             }
         break;
 
         case 3:
             for (let i = 1; i <= 49; i++) {
-                contenitore.innerHTML += `<div class="item item3">${i}</div>`
+                contenitore.innerHTML += `<div class="item3 item">${i}</div>`
             }
         break;
     }
+
+    let caselleTotali = document.querySelectorAll(".item");
+    let bombe = [];
+    let numero;
+    let posizione;
+    
+
+    //inserimento bombe negli elementi della griglia
+    //creazione array per le bombe con il random
+    while (bombe.length < 16) { //controllo array per non ripetere i numeri
+        numero = Math.floor(Math.random() * caselleTotali.length +1);
+        if (bombe.includes(numero)) {
+            
+        }else{
+            bombe.push(numero);
+        }
+        
+    }
+
+    //inserimento click sulla griglia
+    let attivi = document.querySelectorAll(".item"); //seleziono tutti gli elementi della griglia
+    for (let i = 0; i < attivi.length; i++) {
+        //inserisco la funzione di click
+        attivi[i].addEventListener("click", function () {
+            let attivo = this;
+            attivo.classList.add("active");
+
+            //controllo vittoria
+            vittoria(caselleTotali);
+        });
+      
+    }
+
+    
+    console.log(bombe);
+
+    // inserimento bombe
+    for (let i = 0; i < bombe.length; i++) {
+        posizione = bombe[i]-1;
+        caselleTotali[posizione].addEventListener("click", function () {
+            let bomba = this;
+            bomba.classList.add("bomb");
+            sconfitta();
+        });
+    }
+
 });
 
-let attivo = document.querySelector(".item");
-attivo.addEventListener('click', function () {
-    attivo.classList.add("active");
-});
+
+function sconfitta() {
+    let messaggio = document.getElementById("punteggio");
+
+    let punteggio = document.querySelectorAll(".active");
+
+    messaggio.innerHTML = "Hai perso. <br> Il tuo punteggio è di "+ (punteggio.length-1) +" Punti.";
+
+}
+
+function vittoria(caselleTotali) {
+    let punteggio = document.querySelectorAll(".active");
+
+    if (punteggio.length == (caselleTotali.length-16)) {
+        let messaggio = document.getElementById("punteggio");
+        messaggio.innerHTML = "Hai VINTO!! <br> Il tuo punteggio è di "+ (punteggio.length-1) +" Punti.";
+
+    }
+}
