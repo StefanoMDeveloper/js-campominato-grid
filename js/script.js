11f00f7fe2fcafe220061a960cb5c3ef78e0e77f
StefanoMDeveloper <stenmarti02@gmail.com>
2021-12-22 12:48:03 +0100

    bonus, stop gioco

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 /*
-L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+    L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+    con difficoltà 1 => tra 1 e 100
+    con difficoltà 2 => tra 1 e 81
+    con difficoltà 3 => tra 1 e 49
+    Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
 let bottone = document.getElementById("gioca");
-bottone.addEventListener('click', function(){
+bottone.addEventListener('click', function() {
+    let gameOver = false;
     let livello = parseInt(document.getElementById("difficolta").value);
     let contenitore = document.querySelector(".container");
     contenitore.innerHTML = "";
@@ -38,6 +39,7 @@ bottone.addEventListener('click', function(){
     let numero;
     let posizione;
     
+    
 
     //inserimento bombe negli elementi della griglia
     //creazione array per le bombe con il random
@@ -50,34 +52,42 @@ bottone.addEventListener('click', function(){
         }
         
     }
-
-    //inserimento click sulla griglia
-    let attivi = document.querySelectorAll(".item"); //seleziono tutti gli elementi della griglia
-    for (let i = 0; i < attivi.length; i++) {
-        //inserisco la funzione di click
-        attivi[i].addEventListener("click", function () {
-            let attivo = this;
-            attivo.classList.add("active");
-
-            //controllo vittoria
-            vittoria(caselleTotali);
-        });
-      
-    }
-
     
-    console.log(bombe);
+        //inserimento click sulla griglia
+        let attivi = document.querySelectorAll(".item"); //seleziono tutti gli elementi della griglia
+        for (let i = 0; i < attivi.length; i++) {
+                
+            //inserisco la funzione di click
+            attivi[i].addEventListener("click", function () {
+                //controllo che il gioco non sia finito
+                if (gameOver != true) {
+                    let attivo = this;
+                    attivo.classList.add("active");
 
-    // inserimento bombe
-    for (let i = 0; i < bombe.length; i++) {
-        posizione = bombe[i]-1;
-        caselleTotali[posizione].addEventListener("click", function () {
-            let bomba = this;
-            bomba.classList.add("bomb");
-            sconfitta();
-        });
-    }
+                    //controllo vittoria
+                    gameOver = vittoria(caselleTotali);
+                    console.log(gameOver);
+                }
+            });
+        }
 
+        //un bell'array per vincere facile
+        console.log(bombe);
+
+        // inserimento bombe
+        for (let i = 0; i < bombe.length; i++) {
+            posizione = bombe[i]-1;
+
+            caselleTotali[posizione].addEventListener("click", function () {
+                //controllo che il gioco non sia finito
+                if (gameOver != true) {
+                    let bomba = this;
+                    bomba.classList.add("bomb");
+                    gameOver = sconfitta();
+                    console.log(gameOver);
+                }
+            });
+        }
 });
 
 
@@ -88,6 +98,7 @@ function sconfitta() {
 
     messaggio.innerHTML = "Hai perso. <br> Il tuo punteggio è di "+ (punteggio.length-1) +" Punti.";
 
+    return true
 }
 
 function vittoria(caselleTotali) {
@@ -96,6 +107,10 @@ function vittoria(caselleTotali) {
     if (punteggio.length == (caselleTotali.length-16)) {
         let messaggio = document.getElementById("punteggio");
         messaggio.innerHTML = "Hai VINTO!! <br> Il tuo punteggio è di "+ (punteggio.length-1) +" Punti.";
-
+        
+        
+        return true
+    }else{
+        return false
     }
 }
